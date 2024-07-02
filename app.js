a53e35bbe3368a5ffd0d1053857408c95a4ec6e4
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-07-02 09:48:53 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
-setInterval(updateTime, 1000);  // Update every second
 function updateTime() {
             const now = new Date();
             const utcTime = now.toUTCString().split(' ')[4];
-            const utcDate = now.toUTCString().split(' ').slice(0, 4).join(' ');  // Get the first four parts (day, month, date, and year)
-            const day = now.toLocaleDateString('en-US', { weekday: 'long' });
+            const utcDay = now.toUTCString().split(',')[0];
+            const utcDateArray = now.toUTCString().split(' ');
+            const utcDate = `${utcDateArray[1]} ${utcDateArray[2]} ${utcDateArray[3]}`;  // Format as 'Date Month Year'
 
             document.getElementById('time').textContent = utcTime;
-            document.getElementById('day').textContent = day;
+            document.getElementById('day').textContent = utcDay;
             document.getElementById('date').textContent = utcDate;
         }
 
